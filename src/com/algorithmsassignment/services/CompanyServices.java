7e69a3c48f34e28279c6d9e516e3fa050a520b23
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-09-24 20:03:25 +0530

    Added company input (moved from company model)

--- src/com/algorithmsassignment/services/CompanyServices.java
+++ src/com/algorithmsassignment/services/CompanyServices.java
@@ -1,14 +1,29 @@
 package com.algorithmsassignment.services;
 
+import java.util.Scanner;
+
+import com.algorithmsassignment.models.Company;
+
 public class CompanyServices {
+
+    public Company newCompany() {
+        int sharePrice;
+        boolean stockRoseToday;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the share price: ");
+        sharePrice = scanner.nextInt();
+        System.out.print("Whether the company's stock price rose today compared to yesterday? ");
+        stockRoseToday = scanner.nextBoolean();
+        return (new Company(sharePrice, stockRoseToday));
+    }
 
     public void companyOperations(int operationNumber) {
 
-        switch(operationNumber) {
+        switch (operationNumber) {
             case 0:
                 return;
             case 1:
-            stockPricesInAscending();
+                stockPricesInAscending();
             case 2:
                 stockPricesInDescending();
             case 3:
