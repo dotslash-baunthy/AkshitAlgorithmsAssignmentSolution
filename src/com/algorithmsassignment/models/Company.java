cd8e0c867cea2cc34fff613f84ee6643b7399e42
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-09-24 20:02:44 +0530

    Moved input out of Company model

--- src/com/algorithmsassignment/models/Company.java
+++ src/com/algorithmsassignment/models/Company.java
@@ -1,16 +1,11 @@
 package com.algorithmsassignment.models;
 
-import java.util.Scanner;
-
 public class Company {
     double sharePrice;
     boolean stockRoseToday;
-    Scanner scanner = new Scanner(System.in);
 
-    public void companyDetails(int companyNumber) {
-        System.out.print("Enter the current stock price of company " + companyNumber);
-        sharePrice = scanner.nextDouble();
-        System.out.print("Whether the company's stock price rose today compared to yesterday? ");
-        stockRoseToday = scanner.nextBoolean();
+    public Company (int sharePrice, boolean stockRoseToday) {
+        this.sharePrice = sharePrice;
+        this.stockRoseToday = stockRoseToday;
     }
 }
