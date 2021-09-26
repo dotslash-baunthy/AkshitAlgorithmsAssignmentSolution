38a8b6f05278c0c51f60c3f92128ae6edec04eae
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-09-26 17:42:19 +0530

    Added comments Updated searchForSpecificStockPrice

--- src/com/algorithmsassignment/services/CompanyServices.java
+++ src/com/algorithmsassignment/services/CompanyServices.java
@@ -6,18 +6,22 @@ import com.algorithmsassignment.models.Company;
 
 public class CompanyServices {
 
+    // Adding new company into Company array
     public Company newCompany(int companyNumber) {
-        int sharePrice;
+        double sharePrice;
         boolean stockRoseToday;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Getting details of company number " + companyNumber + " -");
         System.out.print("Enter the share price: ");
-        sharePrice = scanner.nextInt();
+        sharePrice = scanner.nextDouble();
         System.out.print("Whether the company's stock price rose today compared to yesterday? ");
         stockRoseToday = scanner.nextBoolean();
+        // Return parameterized constructor which is creating a new Company object with
+        // given values
         return (new Company(sharePrice, stockRoseToday));
     }
 
+    // Main calls this function, choice determines what task is to be done
     public void companyOperations(int choice, Company[] companies, double[] sharePrice, boolean[] stockRoseToday) {
 
         if (choice == 0) {
@@ -31,18 +35,26 @@ public class CompanyServices {
         } else if (choice == 4) {
             numberOfCompaniesWithSameStockStatus(companies, false);
         } else if (choice == 5) {
-            searchForSpecificStockPrice();
+            searchForSpecificStockPrice(sharePrice);
         } else {
             System.out.println("Use an option between 1 to 5");
         }
     }
 
+    // Sort sharePrices array
+    // isAscending determines whether in ascending (if isAscending is false) or
+    // descending (if isAscending is false)
     private void stockPricesSort(double[] sharePrice, boolean isAscending) {
         MergeSort mergeSort = new MergeSort();
+        // New array created so that the actual array isn't messed up
+        // This was created in case we wanted to preserve the indexes (this is not used
+        // but code exists in case an update is to be made)
         double[] sharePriceTemp = sharePrice;
         mergeSort.splitArray(sharePriceTemp, 0, sharePriceTemp.length - 1, isAscending);
     }
 
+    // Print how many companies had higher stock value compared to yesterday (if
+    // isRaised is true) or lower stock value (if isRaised is false)
     private void numberOfCompaniesWithSameStockStatus(Company[] companies, boolean isRaised) {
         int counter = 0;
         for (int i = 0; i < companies.length; i++) {
@@ -59,7 +71,13 @@ public class CompanyServices {
         System.out.println(counter);
     }
 
-    private int searchForSpecificStockPrice() {
-        return 0;
+    // Search for specific stock value
+    // This calls a linearSearch function
+    private void searchForSpecificStockPrice(double[] sharePrice) {
+        LinearSearch linearSearch = new LinearSearch();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the key value: ");
+        double searchValue = scanner.nextDouble();
+        linearSearch.search(sharePrice, searchValue);
     }
 }
