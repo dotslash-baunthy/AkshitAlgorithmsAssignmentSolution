4b2549069b80c40606ae0e530ba0f0f2972b4002
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-09-25 18:53:45 -0700

    Split companies[], operations in separate class

--- src/com/algorithmsassignment/main/Driver.java
+++ src/com/algorithmsassignment/main/Driver.java
@@ -10,17 +10,23 @@ public class Driver {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numberOfCompanies;
-        int choice;
         CompanyServices companyServices = new CompanyServices();
+
+        int choice;
 
         Company[] companies;
 
         System.out.print("Enter the number of companies: ");
         numberOfCompanies = scanner.nextInt();
         companies = new Company[numberOfCompanies];
+
+        double[] sharePrice = new double[numberOfCompanies];
+        boolean[] stockRoseToday = new boolean[numberOfCompanies];
         
         for (int i = 0; i < numberOfCompanies; i++) {
-            companies[i] = companyServices.newCompany();
+            companies[i] = companyServices.newCompany((i+1));
+            sharePrice[i] = companies[i].getSharePrice();
+            stockRoseToday[i] = companies[i].getStockRoseToday();
         }
 
         do {
@@ -32,11 +38,12 @@ public class Driver {
             System.out.println("5. Search a specific stock price");
             System.out.println("6. Press '0' to exit");
             System.out.println("-----------------------------------------------");
+
             choice = scanner.nextInt();
 
-            companyServices.companyOperations(choice);
+            companyServices.companyOperations(choice, companies, sharePrice, stockRoseToday);
 
-        } while (choice != 0);
+        } while(choice != 0);
 
     }
 }
