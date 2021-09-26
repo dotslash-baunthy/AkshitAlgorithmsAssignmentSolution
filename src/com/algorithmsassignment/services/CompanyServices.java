59d6d58d5412daca615adbf798dfd7aa6e757c04
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-09-25 18:56:18 -0700

    Move from switch to if, functions take arguments

--- src/com/algorithmsassignment/services/CompanyServices.java
+++ src/com/algorithmsassignment/services/CompanyServices.java
@@ -2,14 +2,17 @@ package com.algorithmsassignment.services;
 
 import java.util.Scanner;
 
+import javax.management.openmbean.OpenDataException;
+
 import com.algorithmsassignment.models.Company;
 
 public class CompanyServices {
 
-    public Company newCompany() {
+    public Company newCompany(int companyNumber) {
         int sharePrice;
         boolean stockRoseToday;
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Getting details of company number " + companyNumber + " -");
         System.out.print("Enter the share price: ");
         sharePrice = scanner.nextInt();
         System.out.print("Whether the company's stock price rose today compared to yesterday? ");
@@ -17,42 +20,62 @@ public class CompanyServices {
         return (new Company(sharePrice, stockRoseToday));
     }
 
-    public void companyOperations(int operationNumber) {
-
-        switch (operationNumber) {
-            case 0:
-                return;
-            case 1:
-                stockPricesInAscending();
-            case 2:
-                stockPricesInDescending();
-            case 3:
-                numberOfCompaniesWithRaisedStock();
-            case 4:
-                numberOfCompaniesWithDeclinedStock();
-            case 5:
+    public void companyOperations(int choice, Company[] companies, double[] sharePrice, boolean[] stockRoseToday) {
+        
+        if (choice == 0) {
+                System.exit(0);
+            } else if (choice == 1) {
+                stockPricesInAscending(sharePrice);
+            } else if (choice == 2) {
+                stockPricesInDescending(sharePrice);
+            } else if (choice == 3) {
+                numberOfCompaniesWithRaisedStock(companies);
+            } else if (choice == 4) {
+                numberOfCompaniesWithDeclinedStock(companies);
+            } else if (choice == 5) {
                 searchForSpecificStockPrice();
-            default:
+            } else {
                 System.out.println("Use an option between 1 to 5");
-        }
+            }
     }
 
-    private int[] stockPricesInAscending() {
-        int[] ascendingArray = { 0 };
-        return ascendingArray;
+    private void stockPricesInAscending(double[] sharePrice) {
+        MergeSort mergeSort = new MergeSort();
+        double[] sharePriceTemp = sharePrice;
+        mergeSort.splitArray(sharePriceTemp, 0, sharePrice.length);
     }
 
-    private int[] stockPricesInDescending() {
+    private int[] stockPricesInDescending(double[] sharePrice) {
         int[] descendingArray = { 0 };
         return descendingArray;
     }
 
-    private int numberOfCompaniesWithRaisedStock() {
-        return 1;
+    private void numberOfCompaniesWithRaisedStock(Company[] companies) {
+        int stockRoseTodayCounter = 0;
+        for (int i = 0; i < companies.length; i++) {
+            if (companies[i].getStockRoseToday()) {
+                stockRoseTodayCounter++;
+            }
+        }
+        if (stockRoseTodayCounter == 1) {
+            System.out.println(stockRoseTodayCounter + " company had raised stocks");
+        } else {
+            System.out.println(stockRoseTodayCounter + " companies had raised stocks");
+        }
     }
 
-    private int numberOfCompaniesWithDeclinedStock() {
-        return -1;
+    private void numberOfCompaniesWithDeclinedStock(Company[] companies) {
+        int stockDeclinedTodayCounter = 0;
+        for (int i = 0; i < companies.length; i++) {
+            if (!companies[i].getStockRoseToday()) {
+                stockDeclinedTodayCounter++;
+            }
+        }
+        if (stockDeclinedTodayCounter == 1) {
+            System.out.println(stockDeclinedTodayCounter + " company had declined stocks");
+        } else {
+            System.out.println(stockDeclinedTodayCounter + " companies had declined stocks");
+        }
     }
 
     private int searchForSpecificStockPrice() {
